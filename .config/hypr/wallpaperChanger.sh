52d0fe1013f84209ae3d6cc48d4cdcba454b03ef
pkkek <163933152+RO-Owen@users.noreply.github.com>
2026-04-26 11:30:34 -0400

    scaling fit

--- .config/hypr/wallpaperChanger.sh
+++ .config/hypr/wallpaperChanger.sh
@@ -2,5 +2,5 @@
 
 pkill linux-wallpaper
 cd /home/pkkek/.local/share/Steam/steamapps/workshop/content/431960/
-linux-wallpaperengine --fps 16 --screen-root "$(hyprctl monitors | sed -n 's/^Monitor \([^ ]*\).*/\1/p'
+linux-wallpaperengine --scaling fit --fps 16 --screen-root "$(hyprctl monitors | sed -n 's/^Monitor \([^ ]*\).*/\1/p'
 )" "$(ls | shuf -n 1)"
